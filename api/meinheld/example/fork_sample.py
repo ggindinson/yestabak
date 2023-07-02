b9d9983d45ddf400299df9da372c699f24e7205e
enganese <b-android@mail.ru>
2023-07-03 00:01:20 +0500

    Create meinheld

---
+++ api/meinheld/example/fork_sample.py
@@ -0,0 +1,33 @@
+import meinheld
+from multiprocessing import Process
+import signal
+
+workers = []
+
+def hello_world(environ, start_response):
+    status = '200 OK'
+    res = b"Hello world!"
+    response_headers = [('Content-type', 'text/plain'), ('Content-Length', str(len(res)))]
+    start_response(status, response_headers)
+    #print(environ)
+    return [res]
+
+def run(app, i):
+    meinheld.run(app)
+
+def kill_all(sig, st):
+    for w in workers:
+        w.terminate()
+
+def start(num=4):
+    for i in range(num):
+        p = Process(name="worker-%d" % i, target=run, args=(hello_world,i))
+        workers.append(p)
+        p.start()
+
+signal.signal(signal.SIGTERM, kill_all)
+meinheld.set_keepalive(10)
+meinheld.set_access_logger(None)
+meinheld.set_error_logger(None)
+meinheld.listen(("0.0.0.0", 8000))
+start()
